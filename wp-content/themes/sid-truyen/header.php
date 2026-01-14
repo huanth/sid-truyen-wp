4ffe0e3409d6545912d0bb83364db1f025fc5277
Huan Truong <huanth@onextdigital.com>
2026-01-14 23:14:13 +0700

    chore: Remove direct favicon link from header.php.

--- wp-content/themes/sid-truyen/header.php
+++ wp-content/themes/sid-truyen/header.php
@@ -3,7 +3,6 @@
 <head>
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel="icon" href="<?php echo get_template_directory_uri(); ?>/assets/images/favicon.png" type="image/png">
 	<?php wp_head(); ?>
 	<style>
 		/* Fix Header covered by Admin Bar */
